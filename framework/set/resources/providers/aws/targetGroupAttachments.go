af3e5892063241b72a9a7e7d4e99d15851814b7e
Caleb Warren <slickwarren21@gmail.com>
2025-05-06 13:49:35 -0700

    Revert "Merge pull request #119 from markusewalker/markus/task-1800"
    This reverts commit a731edaaa258b6326ad582245fb13e29fea02c75, reversing
    changes made to d660478d5c3a3b7f935ed3f21d0eab5572f70d94.

--- framework/set/resources/providers/aws/targetGroupAttachments.go
+++ framework/set/resources/providers/aws/targetGroupAttachments.go
@@ -5,21 +5,18 @@ import (
 
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/hcl/v2/hclwrite"
-	"github.com/rancher/tfp-automation/config"
 	"github.com/rancher/tfp-automation/framework/set/defaults"
 	"github.com/zclconf/go-cty/cty"
 )
 
 const (
 	eachValue       = "each.value"
-	ipv6EachValue   = "each.value.ipv6_addresses[0]"
 	forEach         = "for_each"
 	rke2InstanceIDs = "rke2_instance_ids"
 )
 
 // CreateTargetGroupAttachments is a function that will set the target group attachments configurations in the main.tf file.
-func CreateTargetGroupAttachments(rootBody *hclwrite.Body, terraformConfig *config.TerraformConfig, lbTargetGroupAttachment,
-	targetGroupAttachmentServer string, port int64) {
+func CreateTargetGroupAttachments(rootBody *hclwrite.Body, lbTargetGroupAttachment, targetGroupAttachmentServer string, port int64) {
 	targetGroupBlock := rootBody.AppendNewBlock(defaults.Resource, []string{lbTargetGroupAttachment, targetGroupAttachmentServer})
 	targetGroupBlockBody := targetGroupBlock.Body()
 
@@ -37,17 +34,9 @@ func CreateTargetGroupAttachments(rootBody *hclwrite.Body, terraformConfig *conf
 
 	targetGroupBlockBody.SetAttributeRaw(defaults.TargetGroupARN, values)
 
-	var targetIDExpression string
-	if !terraformConfig.AWSConfig.EnablePrimaryIPv6 {
-		targetIDExpression = eachValue
-		values = hclwrite.Tokens{
-			{Type: hclsyntax.TokenIdent, Bytes: []byte(targetIDExpression)},
-		}
-	} else {
-		targetIDExpression = ipv6EachValue
-		values = hclwrite.Tokens{
-			{Type: hclsyntax.TokenIdent, Bytes: []byte(targetIDExpression)},
-		}
+	targetIDExpression := eachValue
+	values = hclwrite.Tokens{
+		{Type: hclsyntax.TokenIdent, Bytes: []byte(targetIDExpression)},
 	}
 
 	targetGroupBlockBody.SetAttributeRaw(defaults.TargetID, values)
@@ -55,8 +44,7 @@ func CreateTargetGroupAttachments(rootBody *hclwrite.Body, terraformConfig *conf
 }
 
 // CreateInternalTargetGroupAttachments is a function that will set the internal target group attachments configurations in the main.tf file.
-func CreateInternalTargetGroupAttachments(rootBody *hclwrite.Body, terraformConfig *config.TerraformConfig, lbTargetGroupAttachment,
-	targetGroupAttachmentServer string, port int64) {
+func CreateInternalTargetGroupAttachments(rootBody *hclwrite.Body, lbTargetGroupAttachment, targetGroupAttachmentServer string, port int64) {
 	targetGroupBlock := rootBody.AppendNewBlock(defaults.Resource, []string{lbTargetGroupAttachment, targetGroupAttachmentServer})
 	targetGroupBlockBody := targetGroupBlock.Body()
 
@@ -74,17 +62,9 @@ func CreateInternalTargetGroupAttachments(rootBody *hclwrite.Body, terraformConf
 
 	targetGroupBlockBody.SetAttributeRaw(defaults.TargetGroupARN, values)
 
-	var targetIDExpression string
-	if !terraformConfig.AWSConfig.EnablePrimaryIPv6 {
-		targetIDExpression = eachValue
-		values = hclwrite.Tokens{
-			{Type: hclsyntax.TokenIdent, Bytes: []byte(targetIDExpression)},
-		}
-	} else {
-		targetIDExpression = ipv6EachValue
-		values = hclwrite.Tokens{
-			{Type: hclsyntax.TokenIdent, Bytes: []byte(targetIDExpression)},
-		}
+	targetIDExpression := eachValue
+	values = hclwrite.Tokens{
+		{Type: hclsyntax.TokenIdent, Bytes: []byte(targetIDExpression)},
 	}
 
 	targetGroupBlockBody.SetAttributeRaw(defaults.TargetID, values)
